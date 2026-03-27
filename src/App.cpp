69d134c1ec3e2369fb19835f31e74ec912faeaed
Bernardo Brust <bernardo.toledo.pereira@gmail.com>
2026-03-27 17:53:21 -0300

    Ball

--- src/App.cpp
+++ src/App.cpp
@@ -7,6 +7,7 @@
 #include <raylib.h>
 
 #include "add_vecs.h"
+#include "Ball.h"
 
 constexpr size_t window_width = 800;
 constexpr size_t window_height = 600;
@@ -15,12 +16,24 @@ constexpr size_t fps_target = 240;
 constexpr auto ball_color = RAYWHITE;
 constexpr auto background_color = BLACK;
 
+static std::array balls {
+    Ball(10.0, 10.0, 2.0, 2.0),
+    Ball(50.0, 10.0, 2.0, 2.0),
+    Ball(100.0, 30.0, 2.0, 2.0),
+    Ball(170.0, 50.0, 2.0, 2.0),
+    Ball(400.0, 70.0, 2.0, 2.0),
+};
+
 auto App::tick() -> void {
     // Empty for now
 }
 
 auto App::render() -> void {
     BeginDrawing();
+
+    std::ranges::for_each(std::as_const(balls), [](const auto &e) {
+        DrawCircle(e.get_pos_x(), e.get_pos_y(), Ball::radius, ball_color);
+    });
 
     ClearBackground(background_color);
 
---
+++ src/Ball.h
@@ -0,0 +1,28 @@
+//
+// Created by be on 3/27/26.
+//
+
+#ifndef TELP_T1_BALL_H
+#define TELP_T1_BALL_H
+
+class Ball {
+    float m_pos_x{}, m_pos_y{}, m_vel_x{}, m_vel_y{};
+
+public:
+    static constexpr float radius{6.0};
+
+    Ball(const float x, const float y, const float vel_x, const float vel_y) : m_pos_x{x}, m_pos_y{y}, m_vel_x{vel_x}, m_vel_y{vel_y} {
+    };
+    ~Ball() = default;
+
+    [[nodiscard]] float get_pos_x() const {
+        return m_pos_x;
+    }
+
+    [[nodiscard]] float get_pos_y() const {
+        return m_pos_y;
+    }
+};
+
+
+#endif //TELP_T1_BALL_H
